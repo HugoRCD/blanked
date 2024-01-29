d352d6dbf5e98b75e1748229f9ac04ca9b691080
HugoRCD <hugo.richard@epitech.eu>
2024-01-29 19:04:32 +0100

    Add more meta for seo

--- docs/app.vue
+++ docs/app.vue
@@ -1,6 +1,56 @@
 <script setup lang="ts">
 const { socials, appName } = useAppConfig();
 
+useHead({
+  title: appName + " - A Nuxt Module to improve your developer experience",
+  meta: [
+    {
+      name: 'description',
+      content: 'A Nuxt module to improve your developer experience, give you a head start and make your life easier. Add tailwindcss, toast notifications, dark mode, and more to your Nuxt app in seconds.'
+    },
+    {
+      name: 'keywords',
+      content: 'nuxt, nuxtjs, nuxt.js, nuxt module, nuxt modules, nuxt toast, nuxt tailwindcss, nuxt tailwind, nuxt dark mode, nuxt darkmode, nuxt dark, nuxt color mode, nuxt color-mode, nuxt color-mode-module, nuxt color-mode-module, nuxt color mode module, nuxt color-mode module, nuxt color mode, nuxt color-mode, nuxt color mode-module, nuxt color mode-module, nuxt color mode module, nuxt color-mode module, nuxt color mode, nuxt color-mode, nuxt color mode-module, nuxt color mode-module, nuxt color mode module, nuxt color-mode module, nuxt color mode, nuxt color-mode, nuxt color mode-module, nuxt color mode-module, nuxt color mode module, nuxt color-mode module, nuxt color mode, nuxt color-mode, nuxt color mode-module, nuxt color mode-module, nuxt color mode module, nuxt color-mode module, nuxt color mode, nuxt color-mode, nuxt color mode-module, nuxt color mode-module, nuxt color mode module, nuxt color-mode module'
+    },
+    {
+      name: 'og:title',
+      content: appName + " - A Nuxt Module to improve your developer experience"
+    },
+    {
+      name: 'og:description',
+      content: 'A Nuxt module to improve your developer experience, give you a head start and make your life easier. Add tailwindcss, toast notifications, dark mode, and more to your Nuxt app in seconds.'
+    },
+    {
+      name: 'og:image',
+      content: 'https://blanked.hrcd.fr/blanked-social-preview.png'
+    },
+    {
+      name: 'og:url',
+      content: 'https://blanked.hrcd.fr'
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image'
+    },
+    {
+      name: 'twitter:title',
+      content: appName + " - A Nuxt Module to improve your developer experience"
+    },
+    {
+      name: 'twitter:description',
+      content: 'A Nuxt module to improve your developer experience, give you a head start and make your life easier. Add tailwindcss, toast notifications, dark mode, and more to your Nuxt app in seconds.'
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://blanked.hrcd.fr/blanked-social-preview.png'
+    },
+    {
+      name: 'twitter:url',
+      content: 'https://blanked.hrcd.fr'
+    }
+  ],
+})
+
 const features = [
   {
     title: 'TailwindCSS',
